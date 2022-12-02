58c3b4ace6712be163d2e3a2a0cb80d432f5b824
notmyname <bcboos05@gmail.com>
2022-12-02 14:51:42 +0100

    trying something funny

---
+++ src/cogs/neko.py
@@ -0,0 +1,21 @@
+import discord, io, aiohttp, json, requests
+from logging import info as logging_info
+from discord.ext import commands
+
+async def image(link):
+	logging_info(f"image function ran with {link}")
+	async with aiohttp.ClientSession() as session:
+		async with session.get(link) as resp:
+			json_data = json.loads(await resp.text())
+			async with session.get(json_data["url"]) as resp:
+				return io.BytesIO(await resp.read())
+
+class Misc(commands.Cog):
+	def __init__(self, client) -> None:
+		self.client: commands.Bot  = client
+		return
+
+	@discord.slash_command(name= "waifu", description="Get one NSFW pic.")
+	@commands.is_nsfw()
+	async def waifu(self, interaction:discord.Interaction) -> None:
+		await interaction.response.send_message(file=discord.File(await image("https://api.waifu.pics/nsfw/waifu"), "waifu.png"))
